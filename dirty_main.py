6fe915aba26a3a3c4c0e850fce887b03387f7b75
Azoxe <semenuchkin@yandex.ru>
2023-06-08 19:17:02 +0300

    second

--- dirty_main.py
+++ dirty_main.py
@@ -1,1 +1,10 @@
-dirty_main.py
+from datetime import datetime, date, time
+import application.salary    
+from application import salary
+import application.db.people
+from application.db import people
+
+if __name__ == '__main__':
+    print(datetime.now(tz=None), salary.calculate_salary(1))
+    print(datetime.now(tz=None), people.get_employees(1))
+    
